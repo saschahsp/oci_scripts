fbc715161f61a469047cf3094e4afa8f2c00e263
saschahsp <saschah@gmx.net>
2020-06-12 15:05:45 +0200

    blub

--- users2adw.py
+++ users2adw.py
@@ -263,8 +263,8 @@ def main_process():
     try:
         secret_id = 'ocid1.vaultsecret.oc1.eu-frankfurt-1.amaaaaaamfkspnqanwpezodbqjdyfjpkzqelc2o7zkmb3htlsdwwpr3ixxtq'
         secret_bundle = SecretsClient(config, signer=signer).get_secret_bundle(secret_id)
-        base64.b64decode(secret_bundle.data.secret_bundle_content.content)
-        dpass = print("\nConnecting to database " + cmd.dname)
+        dpass = base64.b64decode(secret_bundle.data.secret_bundle_content.content)
+        print("\nConnecting to database " + cmd.dname)
         logging.info("\nConnecting to database " + cmd.dname)
         connection = cx_Oracle.connect(user=cmd.duser, password=dpass, dsn=cmd.dname, encoding="UTF-8", nencoding="UTF-8")
         cursor = connection.cursor()
